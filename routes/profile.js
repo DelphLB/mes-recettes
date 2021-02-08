3242df1ba84d0a38f3f4bce1f23fade5f65f1cd6
Delphine Le Brun <delphine.clebrun@gmail.com>
2021-02-08 09:43:23 +0100

    secret key

--- routes/profile.js
+++ routes/profile.js
@@ -3,9 +3,18 @@ const router = express.Router();
 const connection = require("../config");
 const jwt = require("jsonwebtoken");
 
+router.post("/", verifyToken, (req, res) => {
+  jwt.verify(req.token, process.env.SECRET_KEY_JWT, (err, result) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
 function verifyToken(req, res, next) {
   const bearerHeader = req.headers["authorization"];
-
   if (typeof bearerHeader !== "undefined") {
     const bearer = bearerHeader.split(" ");
     const bearerToken = bearer[1];
@@ -13,18 +22,5 @@ function verifyToken(req, res, next) {
     next();
   }
 }
-
-router.post("/", verifyToken, (req, res, next) => {
-  jwt.verify(req.token, process.env.SECRET_KEY_JWT, (err, users) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.json({
-        name: users.result[0].name,
-        lastname: users.result[0].nickname,
-      });
-    }
-  });
-});
 
 module.exports = router;
